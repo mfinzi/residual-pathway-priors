43c1e02d7d27e8cba0bbb720b319e702e2c460a1
Marc Finzi <mfinzi@hmc.edu>
2021-04-05 20:48:51 -0400

    copied over some files from emlp

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+import sys, os
+
+setup(name="Residual Pathway Priors",
+      description="",
+      version='0.0',
+      author='-',
+      author_email='maf820@nyu.edu',
+      license='MIT',
+      python_requires='>=3.6',
+      install_requires=['h5py','objax','pytest',
+      'olive-oil-ml @ git+https://github.com/mfinzi/olive-oil-ml',
+      'emlp @ git+https://github.com/mfinzi/equivariant-MLP','optax','tqdm>=4.38'],
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+)
